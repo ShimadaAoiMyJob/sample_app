81ca65193b88f8625c15423373907433841efac3
ShimadaAoi <shimada.aoi.job@gmail.com>
2026-04-26 22:20:28 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    #debugger書くと処理が一時止まる
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save#保存の成功
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render "new",status: :unprocessable_entity
+      #render : この画面(ビュー)を表示する
+      #users/new.html.erbを表示
+    end
+  end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
